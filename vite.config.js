ff5b78b661cfb101a6dd02ee3896d16891793ca0
K.L.T. Lakchani <cst21040@std.uwu.ac.lk>
2025-11-12 22:28:02 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react()
-, tailwindcss()
-  ],
+  base: './',   // ✅ relative paths
+  plugins: [react(), tailwindcss()],
 })
